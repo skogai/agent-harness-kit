0a0ccb6bdc789aebc68725b8fa98d67a4ed03466
enmanuelmag <enmanuelmag@cardor.dev>
2026-05-08 00:41:25 -0500

    feat(form): implement cliFormWithRetry for enhanced form handling with validation

---
+++ src/utils/form.ts
@@ -0,0 +1,19 @@
+import * as p from '@clack/prompts'
+import * as v from 'valibot'
+
+
+export const cliFormWithRetry = async <T>(
+  formFn: () => Promise<T>,
+  schema: v.GenericSchema,
+): Promise<T> => {
+  while (true) {
+    const res = await formFn()
+    const result = v.safeParse(schema, res)
+
+    if (result.success) return result.output as T
+
+    const messages = result.issues.map((i) => i.message).join(', ')
+    p.log.error(messages)
+    p.log.info('Please try again.\n')
+  }
+}
